e6ca94890091bbd239d5817b6a51c27228ff7426
indirapimpalkhare <45708748+indirapimpalkhare@users.noreply.github.com>
2020-11-24 21:17:10 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$username = "jev5ofki0f28120n";
+$password = "fd9mzte2a6h847ai";
 $db = "chromatus_test";
 
 // Create connection
